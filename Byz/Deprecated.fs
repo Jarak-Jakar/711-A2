c1c8a103e744e1363c46f75e49504b8b563399a3
James Cooper <jarak_jakar@hotmail.com>
2016-09-30 10:58:31 +1300

    Abandonded first project, created second...

--- Byz/Deprecated.fs
+++ Byz/Deprecated.fs
@@ -4,28 +4,41 @@
 open System
 open System.IO
 
-type TreeElement(elemVal, otherNodes, rootNode, ?someTEs) as self =
-    let elemVal = elemVal
+type TreeElement(elemVal, otherNode, rootNode:Node, ?someTEs) as self =
+    //let elemVal = elemVal
 
-    let otherNodes = otherNodes
+    //let otherNodes = otherNodes
 
-    let rootNode = rootNode
+    //let rootNode = rootNode
 
     let mutable subTEs = 
         match someTEs with
         | Some (x:TreeElement[]) -> x
         | None -> [||]
 
-    let rec stringifyTree (tree:TreeElement) = 
+    (*let rec stringifyTree (tree:TreeElement) = 
         let mutable retval = ""
         if subTEs.Length < 1 then
             if elemVal = "0" || elemVal = "1" then
                 retval <- elemVal
             else 
                 //retval <- rootNode.V0
-                retval <- "0"
+                retval <- rootNode.V0
         else
-            let returnString = String.concat "" [| for x in subTEs -> (stringifyTree x) |]
+            let returnString = String.concat "." [| for x in subTEs -> (stringifyTree x) |]
+            retval <- returnString
+
+        retval*)
+
+    let rec stringifyTree (tree:TreeElement) = 
+        let mutable retval = ""
+        if subTEs.Length < 1 then
+            if elemVal = "0" || elemVal = "1" then
+                retval <- ":" + elemVal
+            else 
+                retval <- ":" + rootNode.V0
+        else
+            let returnString = rootNode.NodeID.ToString() + "." + (String.concat "." [| for x in subTEs -> (stringifyTree x) |])
             retval <- returnString
 
         retval
@@ -33,12 +46,14 @@ type TreeElement(elemVal, otherNodes, rootNode, ?someTEs) as self =
     member x.AttachTree tree = 
         subTEs <- tree
 
+    member x.AttachTreeRet tree = 
+        subTEs <- tree
+        self
+
     member x.GetTreeString = 
         stringifyTree self
 
-    
-
-type Node(nodeID, initVal, isFaulty, ?someMessages, ?somev0) =
+and Node(nodeID, initVal, isFaulty, ?someMessages, ?somev0) as self =
     let nodeID = Int32.Parse nodeID
 
     let messages =
@@ -48,15 +63,21 @@ type Node(nodeID, initVal, isFaulty, ?someMessages, ?somev0) =
 
     let v0 = 
         match somev0 with
-        | Some x -> Int32.Parse x
-        | None -> 4
+        | Some x -> x
+        | None -> "4"
 
     let initVal = initVal
 
-    let isFaulty = isFaulty
+    //let isFaulty = isFaulty
+
+    member val Tree = TreeElement(v0, "bob", self) with get, set
 
     member x.NodeID
         with get () = nodeID
+
+    (*member x.Tree
+        with get () = Tree
+        and set y = Tree <- y*)
 
     member x.V0
         with get () = v0
@@ -72,8 +93,8 @@ let main argv =
     let mutable linePieces = fileLine.Split()
     let numNodes = Int32.Parse linePieces.[0]
     let v0 = linePieces.[1]
-    let (nodeArray:Node[]) = Array.create numNodes (Node("a", "b", "c"))
-    for i = 0 to numNodes do
+    let (nodeArray:Node[]) = Array.create numNodes (Node("0", "1", "2"))
+    for i = 0 to numNodes - 1 do
         fileLine <- reader.ReadLine()
         linePieces <- fileLine.Split()
         if linePieces.[2] = "1" then
@@ -83,7 +104,12 @@ let main argv =
 
     Array.sortInPlaceBy (fun (x:Node) -> x.NodeID) nodeArray
 
+    let leaves = [|for x in 0..10 -> TreeElement(x.ToString(), "fred", nodeArray.[0]) |]
 
+    let top = Array.reduce (fun x (y:TreeElement) -> y.AttachTreeRet [|x|]) leaves
 
+    let mystring = top.GetTreeString
+    
+    printf "%A" mystring
 
     0 // return an integer exit code
